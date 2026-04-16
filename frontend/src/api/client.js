d8eb41f2d11be00a87339944fee5fbfc5df81ea2
kn1ghtOwl <vinayak03.2003@gmail.com>
2026-04-16 19:25:26 +0530

    frontend prod

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://primetrade-backend-assingnment.onrender.com/api',
   withCredentials: true
 })
 
